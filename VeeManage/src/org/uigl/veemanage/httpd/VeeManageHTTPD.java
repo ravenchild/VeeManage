02e6ecc153088effcea1cd7a57bd2fac567f9743
Eric Roth <rothes.work@gmail.com>
2012-10-09 16:11:37 -0500

    Added a session manager.

--- VeeManage/src/org/uigl/veemanage/httpd/VeeManageHTTPD.java
+++ VeeManage/src/org/uigl/veemanage/httpd/VeeManageHTTPD.java
@@ -2,6 +2,7 @@ package org.uigl.veemanage.httpd;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import fi.iki.elonen.NanoHTTPD.NanoHTTPD;
@@ -12,14 +13,18 @@ public class VeeManageHTTPD extends NanoHTTPD {
 	public static final int WWW_PORT = 8080;
 	
 	private File mRoot;
+	private SessionManager mSessions;
 	
 	public VeeManageHTTPD(int port, File wwwroot) throws IOException {
 		super(WWW_PORT, wwwroot);
 		this.mRoot = wwwroot;
+		this.mSessions = new SessionManager();
 	}
 	
 	@Override
 	public Response serve(String uri, String method, Properties header, Properties parms, Properties files) {
+		
+		
 		
 		if (uri.equals("/")) {
 			return new Response(HTTP_OK, MIME_HTML, "<html><body>Index</body></html>");
