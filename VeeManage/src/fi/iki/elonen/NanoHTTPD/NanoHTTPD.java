4b54276e8c9f9fd243ed8f1563e5f30c3102beb4
ravenchild <rothe@uigl.org>
2012-12-16 23:06:46 -0600

    Added code to handle multiple SSL records in request. (Need to check this)

--- VeeManage/src/fi/iki/elonen/NanoHTTPD/NanoHTTPD.java
+++ VeeManage/src/fi/iki/elonen/NanoHTTPD/NanoHTTPD.java
@@ -477,6 +477,10 @@ public class NanoHTTPD
 			try {
 				// Read the request line
 				String inLine = in.readLine();
+				if (inLine == null || inLine.length() == 1) {
+					in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+					inLine = inLine + in.readLine();
+				}
 				if (inLine == null) return;
 				StringTokenizer st = new StringTokenizer( inLine );
 				if ( !st.hasMoreTokens())
